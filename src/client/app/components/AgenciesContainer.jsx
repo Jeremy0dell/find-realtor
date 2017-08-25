e70308c8b6e55a6f053dc88207f8d9a5a2dbb2ab
Jeremy Odell <jeremy0dell@yahoo.com>
2017-08-24 23:16:19 -0500

    finish sorted list

--- src/client/app/components/AgenciesContainer.jsx
+++ src/client/app/components/AgenciesContainer.jsx
@@ -3,7 +3,7 @@ import AgenciesList from './AgenciesList.jsx'
 import AgenciesSearch from './AgenciesSearch.jsx'
 import { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 
-import { getMapInfo, getAgencyInfo } from '../util.js'
+import { getMapInfo, getAgencyInfo, arrayUnique, sortByDistance } from '../util.js'
 
 import axios from 'axios'
 
@@ -31,48 +31,19 @@ class AgenciesContainer extends Component {
         return data
       })
       .then(getAgencyInfo)
-      .then((res) => {
-        console.log(res, 'WOW')
+      .then(arrayUnique)
+      .then((data) => {
+        this.setState({ nearbyAgencies: sortByDistance(data, this.state.coordinates[0], this.state.coordinates[1]) })
       })
-
-      // geocodeByAddress(this.props.addresses.address0)
-      //   .then(results => getLatLng(results[0]))
-      //   .then(latLng => {
-      //     this.setState({ coordinates: [ latLng, this.state.coordinates[1] ] })
-      //   })
-      //   .catch(error => console.error('Error', error))
-      //
-      // geocodeByAddress(this.props.addresses.address1)
-      //   .then(results => getLatLng(results[0]))
-      //   .then(latLng => {
-      //     this.setState({ coordinates: [ this.state.coordinates[0], latLng ] })
-      //   })
-      //   .catch(error => console.error('Error', error))
     }
-    //
-    // this.fetchAgencies = (coordinates) => {
-    //   let agencies0
-    //   let agencies1
-    //
-    //   console.log(coordinates[0].lat)
-    //   axios({
-    //     method:'get',
-    //     url:`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coordinates[0].lat},${coordinates[0].lng}&radius=500&type=real_estate_agency&key=AIzaSyAsc823zMjgiOy9b1beWBvKUmysOVYRqu4`
-    //   })
-    //   .then((result) => {
-    //     agencies0 = result.data
-    //   })
-    //   .then(() => {
-    //
-    //   })
-    // }
+
   }
 
   render() {
     return (
       <div>
         <AgenciesSearch handleFormSubmit={this.handleFormSubmit}/>
-        <AgenciesList coordinates={this.state.coordinates} />
+        <AgenciesList nearbyAgencies={this.state.nearbyAgencies} />
       </div>
     )
   }
--- src/client/app/util.js
+++ src/client/app/util.js
@@ -47,4 +47,73 @@ function getMapInfo(adr0, adr1) {
   .catch((err) => { console.log('There was an error gathering user information:', err) })
 }
 
-export { getMapInfo, getAgencyInfo }
+function arrayUnique(arr) {
+    var a = arr[0].results.concat(arr[1].results)
+    console.log(a)
+    for(var i=0; i<a.length; ++i) {
+        for(var j=i+1; j<a.length; ++j) {
+            if(a[i].id === a[j].id)
+                a.splice(j--, 1)
+        }
+    }
+    console.log(a)
+    return a
+}
+
+function getDistance(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
+function getTotalDist(element, coord0, coord1) {
+  let arrLat = element.geometry.location.lat
+  let arrLng = element.geometry.location.lng
+  let dist0 = getDistance(arrLat, arrLng, coord0.lat, coord0.lng)
+  let dist1 = getDistance(arrLat, arrLng, coord1.lat, coord1.lng)
+  let totalDist = dist0 + dist1
+  return totalDist
+}
+
+function compare(a,b) {
+  if (a.distance < b.distance)
+     return -1;
+  if (a.distance > b.distance)
+    return 1;
+  return 0;
+}
+
+function sortByDistance(arr, coord0, coord1) {
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i]["distance"] = getTotalDist(arr[i], coord0, coord1)
+  }
+  console.log(arr.map(x => x.distance), arr)
+  arr.sort(function(a, b) {
+    return a.distance - b.distance;
+  });
+  console.log(arr.map(x => x.distance),'now?', arr)
+  // console.log(arr, 'hi')
+  // var result = arr.sort((a, b) => {
+  //   console.log(getTotalDist(a, coord0, coord1) - getTotalDist(b, coord0, coord1))
+  //   return getTotalDist(a, coord0, coord1) - getTotalDist(b, coord0, coord1)
+  // })
+  //
+  // console.log(result, 'ho')
+
+  return arr
+}
+
+export { getMapInfo, getAgencyInfo, arrayUnique, sortByDistance }
