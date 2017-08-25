5524f2f7700bac03cc046ded1de0d82b52d9e7c3
Jeremy Odell <jeremy0dell@yahoo.com>
2017-08-25 15:19:01 -0500

    finish for now

--- src/client/app/components/AgenciesList.jsx
+++ src/client/app/components/AgenciesList.jsx
@@ -17,11 +17,13 @@ class AgenciesList extends Component {
             {nearbyAgencies.length ? <tr>
               <th>Company</th>
               <th>Address</th>
+              <th>Combined Distance</th>
             </tr> : <tr></tr>}
           {nearbyAgencies.map((agency) =>
             <tr key={agency.id}>
               <td>{agency.name}</td>
               <td>{agency.vicinity}</td>
+              <td>{agency.distance}</td>
             </tr>
           )}
         </tbody>
